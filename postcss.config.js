eb6d9b8a88edf403b72433d908281d87c3c59918
Morteza Jamshidi <devmor.j@gmail.com>
2022-04-02 00:13:17 +0430

    🔧 Use advanced options for cssnano in postcss

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,14 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    ...(process.env.NODE_ENV === 'production' ? { cssnano: { preset: 'default' } } : {}),
-    // ...(process.env.NODE_ENV === 'production' ? { cssnano: { preset: 'advanced' } } : {}),
+    // ...(process.env.NODE_ENV === 'production' ? { cssnano: { preset: 'default' } } : {}),
+    ...(process.env.NODE_ENV === 'production' ? {
+      cssnano: {
+        preset: ['advanced', {
+          discardComments: { removeAll: true },
+          cssDeclarationSorter: { order: 'smacss' },
+        }]
+      }
+    } : {}),
   }
 }
